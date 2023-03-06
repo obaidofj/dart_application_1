789329f52d4b71e1d1c3a4ee99b2ee246aea28db
obaidofj <obaidofj@gmail.com>
2023-03-06 14:59:59 +0200

    1

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -20,13 +20,21 @@ void main(List<String> arguments) {
   dyVr=9;
   var age2= 9;
 
-  List<String> maleNames=['name1','name2'];
+  List<String> maleNames=['name1','name2','name1'];
   List<String> femalNames=['fname1','fname2'];
   List<String> allNames=[...maleNames,...femalNames];
   allNames.add("name3");
   allNames.addAll(["name4","name5"]);
 
-  print(allNames);
+  //print(allNames);
 
+
+  allNames.forEach((e) { print(e); });
+
+  List<int> namesLeng = allNames.map ( (e) { return e.length; } ).toList();
+  
+  print(namesLeng);
+  print(allNames.lastIndexOf('name1'));
+  print(allNames.every((e) => e.length>10));
 
 }
